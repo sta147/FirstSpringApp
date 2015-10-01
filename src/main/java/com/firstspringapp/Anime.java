efa653e5b36348ebea288eb4d6af4dc1f8e66b57
Saqib Kayani <s.kayani@building-blocks.com>
2015-10-01 15:42:56 +0100

    Clean up code for the Anime class.

--- src/main/java/com/firstspringapp/Anime.java
+++ src/main/java/com/firstspringapp/Anime.java
@@ -2,7 +2,9 @@ package com.firstspringapp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
+@ToString(includeFieldNames=true)
 @AllArgsConstructor
 public @Data class Anime {
 	private int id;
